9aec4cfc1d354eea20b99f967c3d00e8671d1c88
yaolijun <yaolijun2012@sina.com>
2017-07-22 17:09:21 +0800

    hello测试

---
+++ eyes/src/main/java/com/eyes/pojo/HelloReq.java
@@ -0,0 +1,9 @@
+package com.eyes.pojo;/**
+ * Created by yaolijun on 2017/7/22.
+ */
+
+/**
+ * Created by yaolijun on 2017/7/22 17:07
+ */
+public class HelloReq {
+}
---
+++ eyes/src/main/java/com/eyes/services/HelloServices.java
@@ -0,0 +1,9 @@
+package com.eyes.services;/**
+ * Created by yaolijun on 2017/7/22.
+ */
+
+/**
+ * Created by yaolijun on 2017/7/22 17:08
+ */
+public class HelloServices {
+}
